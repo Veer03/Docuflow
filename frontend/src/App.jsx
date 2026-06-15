1fc587474b492526c12d0a560ef43eb40b057b97
Veerpartap Singh <veerpartapp02@gmail.com>
2026-06-15 14:31:06 -0700

    react card component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,14 @@
 import Header from "./Header";
 import Footer from "./Footer";
+import Card from "./Card";
+
 function App() {
   return (
     <>
       <Header></Header>
+      <Card />
+      <br />
+      <Card />
       <Footer></Footer>
     </>
   );
